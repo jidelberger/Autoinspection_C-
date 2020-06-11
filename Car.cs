84077213e738bdea44624da987ca7df30ae64be9
Jens Idelberger <66440573+jidelberger@users.noreply.github.com>
2020-06-11 21:49:07 +0200

    Add files via upload
    Quellcode und Projekt

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AutoInspect
+{
+    public class Car
+    {
+        public string carName = "Audi";
+        public int baujahr = 1995;
+        public int lastInspectionYear = 2013;
+        //Konstruktor
+        public Car(string newName, int newBauJahr)
+        {
+            carName = newName;
+            baujahr = newBauJahr;
+            
+        }
+        // Methoden
+        public void Inspect()
+        {
+            lastInspectionYear = 2020;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AutoInspect
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Array von Autos
+            Car[] cars = new Car[6];
+            cars[0] = new Car("Mercedes G", 2008);
+            cars[1] = new Car("BMW 3", 2001);
+            cars[2] = new Car("Audi A6", 2005);
+            cars[3] = new Car("Audi A8", 2012);
+            cars[4] = new Car("Hyundai", 2003);
+            cars[5] = new Car("Honda Civic", 1995);
+
+            Console.WriteLine("Bitte geben Sie einen Suchbegriff ein");
+            string searchTerm = Console.ReadLine();
+            Console.WriteLine("Suchbegriff ist: " + searchTerm);
+            Console.WriteLine("Folgende Autos wurden gefunden:");
+
+            foreach(Car car in cars)
+            {
+                if(car.carName.Contains(searchTerm))
+                {
+                    Console.WriteLine(car.carName + " Baujahr: " + car.baujahr + " Letzte Inspection: " + car.lastInspectionYear);
+                    car.Inspect();
+                    Console.WriteLine("Neues Inspektionsdatum: " + car.lastInspectionYear);
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Main();
+        }
+    }
+}
